86aad5a99931d67a20abf76ea0bc266ed596b4c5
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-20 05:10:40 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  new_sentence = []
+  new_sentence << sentence.split
+  new_sentence.each do |word|
+    new_sentence << word.reverse
+  end
+#  new_sentence
+end  
